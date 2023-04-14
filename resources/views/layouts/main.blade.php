2c937468aca975df1f84865f319def60a766a11f
RamdanN <ramnug45@gmail.com>
2023-04-14 02:19:01 -0700

    menambahkan meta dan thumbnail website

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <meta name="keywords"
+        content="umroh, ibadah umroh, Mekah, Madinah, perusahaan penyedia jasa umroh, hidayatur, islam, islami, muslim, travel">
+    <meta name="description"
+        content="Kami adalah partner terpercaya dalam menjalankan ibadah umroh Anda dengan pelayanan yang terbaik dan berkualitas.">
+    <meta name="author" content="Contech.id">
+    <meta name="language" content="id">
+    <meta name="robots" content="index, follow">
+    <meta name="revisit-after" content="7 days">
+    <meta name="copyright" content="Kembangin Teknologi Kita">
+    <meta name="geo.placename" content="Indonesia">
+    <meta name="contact" content="contech.id@gmail.com">
+
+    <meta property="og:title" content="Hidayatur, A Journey For Iman - Hidayatur">
+    <meta property="og:description"
+        content="Kami adalah partner terpercaya dalam menjalankan ibadah umroh Anda dengan pelayanan yang terbaik dan berkualitas.">
+    <meta property="og:image" content="{{ asset('assets/img/thumbnail.jpg') }}">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://hidayatur.id">
 
     {{-- FAVICON --}}
     <link rel="shortcut icon" href="{{ asset('assets/img/logo/favicon.ico') }}" type="image/x-icon">
